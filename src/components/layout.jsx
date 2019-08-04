8cfb16b34accbb2115e77857af2c403f49f3dde2
Alejandro Martinez <hello@alejandrom.xyz>
2019-08-04 11:34:22 -0700

    Gatsby Netlify CMS

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -16,19 +16,7 @@ export default class MainLayout extends React.Component {
       <div>
         <SEO />
         <link href="https://fonts.googleapis.com/css?family=Inconsolata:400,700" rel="stylesheet" />
-        <script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
         {children}
-        <script>
-          if (window.netlifyIdentity) {
-            window.netlifyIdentity.on("init", user => {
-              if (!user) {
-                window.netlifyIdentity.on("login", () => {
-                  document.location.href = "/admin/";
-                });
-              }
-            })
-          }
-        </script>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" />
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" />
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" />
